976b493283a374eb476360d4cb36372545cacca0
Luis Carlos M <luismantilla99@outlook.com>
2019-06-28 11:36:35 -0500

    terminado hasta el 5

---
+++ S4C4/factorial.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int fact(int n){
+
+	if(n>1){
+		return fact(n-1)*n;
+	}else if (n=1){
+		return 1;
+}
+
+
+}
+
+int main(){
+
+	cout << "El valor de 7 factorial es:" << fact(7) <<endl;
+	cout << "El valor de 77 factorial es:" << fact(77) <<endl;
+	
+	return 0;
+}
