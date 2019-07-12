d155eab63da66ab52352b0e7035e52f0da91228b
Luis Carlos M <luismantilla99@outlook.com>
2019-07-12 01:07:19 -0500

    las 3 condiciones implementadas con sus temps promedio

--- MantillaLuisS6C4/Difusion.cpp
+++ MantillaLuisS6C4/Difusion.cpp
@@ -19,6 +19,10 @@ int main(){
 
 	float tempPer[grilla][grilla];
 	float tempNuevaPer[grilla][grilla];
+
+	float tempAb[grilla][grilla];
+	float tempNuevaAb[grilla][grilla];
+
 	
 	float dt = 0.1*dx*dx/nu;
 
@@ -29,6 +33,7 @@ int main(){
 
 	float tempPromedio[veces];
 	float tempPromedioPer[veces];
+	float tempPromedioAb[veces];
 
 
 	//condiciones iniciales
@@ -38,10 +43,12 @@ int main(){
 			if(i*dx<=0.8 && i*dx>=0.6 && j*dx<=0.6 && j*dx>=0.4){
 				temp[i][j] = 100;
 				tempPer[i][j]=100;
+				tempAb[i][j]=100;
 			}
 			else{
 				temp[i][j]= 50;
 				tempPer[i][j]=50;			
+				tempAb[i][j]=50;			
 			}
 		}
 	}
@@ -53,37 +60,68 @@ int main(){
 	
 	for(int l=0; l<veces;l++){
 		float tempMean = 0;
-	//	float tempMeanPer = 0;
+		float tempMeanPer = 0;
+		float tempMeanAb =0;
 		if(l==0 || l*dt==100 || l==veces-1){
 			for(int i =0; i<grilla; i++){
 				for(int j=0; j<grilla; j++){
-					outfile << i*dx << " "<<j*dx <<" "<<temp[i][j]<< endl;	
+					outfile << i*dx << " "<<j*dx <<" "<<temp[i][j]<<" "<<tempPer[i][j]<< " "<<tempAb[i][j]  <<endl;	
 				}
 			}
 		}
 	
 		for(int i =0; i<grilla; i++){
 			for(int j=0; j<grilla; j++){
-			//	tempMeanPer = tempMeanPer + tempPer[i][j];
 			
 				if(i==0 || j==0 || i==grilla-1 || j==grilla-1){
-					
+					int inNew = i-1;
+					int ipNew = i+1;
+					int jnNew = j-1;
+					int jpNew = j+1;
+
+					int inN = i-1;
+					int ipN = i+1;
+					int jnN = j-1;
+					int jpN = j+1;
+
+					if(i==0){inNew = grilla-1; inN=i;}
+					if(i==grilla-1){ipNew = 0; ipN=i;}
+					if(j==0){jnNew = grilla-1; jnN =j;}
+					if(j==grilla-1){jpNew = 0; jpN = j;}
+
 					tempNueva[i][j] = 50;
-				//	tempNuevaPer[i][j]= evol(temp[i][j], temp[i+1][j], temp[i-1][j], temp[i][j+1], temp[i][j-1], cte);	
+					tempNuevaPer[i][j]= evol(tempPer[i][j], tempPer[ipNew][j], tempPer[inNew][j], tempPer[i][jpNew], tempPer[i][jnNew], cte);
+					tempNuevaAb[i][j]= evol(tempAb[i][j], tempAb[ipN][j], tempAb[inN][j], tempAb[i][jpN], tempAb[i][jnN], cte);
 				}
 				else{
 					tempNueva[i][j]= evol(temp[i][j], temp[i+1][j], temp[i-1][j], temp[i][j+1], temp[i][j-1], cte);
+					tempNuevaPer[i][j]= evol(tempPer[i][j], tempPer[i+1][j], tempPer[i-1][j], tempPer[i][j+1], tempPer[i][j-1], cte);
+					tempNuevaAb[i][j]= evol(tempAb[i][j], tempAb[i+1][j], tempAb[i-1][j], tempAb[i][j+1], tempAb[i][j-1], cte);
 				}				
 			}
 		}
 		for(int i =0; i<grilla; i++){
 			for(int j=0; j<grilla; j++){
 				tempMean = tempMean + temp[i][j];
+				tempMeanPer = tempMeanPer + tempPer[i][j];
+				tempMeanAb = tempMeanAb + tempAb[i][j];
 				temp[i][j] = tempNueva[i][j];
+				tempPer[i][j] = tempNuevaPer[i][j];
+				tempAb[i][j] = tempNuevaAb[i][j];
 			}
 		}
 		tempPromedio[l]=tempMean/(grilla*grilla);
+		tempPromedioPer[l] = tempMeanPer/(grilla*grilla);
+		tempPromedioAb[l]=tempMeanAb/(grilla*grilla);
 	}
+	outfile.close();
+
+	outfile.open("temperaturaMean.dat");
+	
+	for(int l=0; l<veces;l++){
+		outfile<< tempPromedio[l] <<" " <<tempPromedioPer[l] <<" "<<tempPromedioAb[l]<< endl;
+	}
+
 	outfile.close();
 	
 	return 0;
--- MantillaLuisS6C4/Plots_Difusion.py
+++ MantillaLuisS6C4/Plots_Difusion.py
@@ -6,19 +6,27 @@ import matplotlib.animation as animation
 from mpl_toolkits.mplot3d import Axes3D
 
 datos = np.genfromtxt("temps.dat")
+promeds = np.genfromtxt("temperaturaMean.dat")
 
 posx = datos[:10000,0]
 posy = datos[:10000,1]
 temp = datos[:10000,2]
+tempPer = datos[:10000,3]
+tempAb =datos[:10000,4]
+
 
 posx1 = datos[10000:20000,0]
 posy1 = datos[10000:20000,1]
 temp1 = datos[10000:20000,2]
+temp1Per = datos[10000:20000,3]
+temp1Ab =  datos[10000:20000,4]
 
 posx2 = datos[20000:30000,0]
 posy2 = datos[20000:30000,1]
 temp2 = datos[20000:30000,2]
-
+temp2Per = datos[20000:30000,3]
+temp2Ab = datos[20000:30000,4]
+##########################################
 figt = plt.figure()
 axt = figt.add_subplot(111,projection='3d')
 
@@ -51,3 +59,101 @@ axt2.set_xlabel("x [m]")
 axt2.set_ylabel("y [m]")
 axt2.set_zlabel("Temperatura")
 plt.savefig("temp2500s.png")
+######################################################
+
+figtp = plt.figure()
+axtp = figtp.add_subplot(111,projection='3d')
+
+
+axtp.plot_trisurf(posx,posy,tempPer, cmap = 'plasma')
+axtp.set_xlabel("x [m]")
+axtp.set_ylabel("y [m]")
+axtp.set_zlabel("Temperatura")
+plt.savefig("temp0sPeriodica.png")
+
+
+
+figt1p = plt.figure()
+axt1p = figt1p.add_subplot(111,projection='3d')
+
+
+axt1p.plot_trisurf(posx1,posy1,temp1Per,cmap = 'plasma')
+axt1p.set_xlabel("x [m]")
+axt1p.set_ylabel("y [m]")
+axt1p.set_zlabel("Temperatura")
+plt.savefig("temp100sPeriodica.png")
+
+
+figt2p = plt.figure()
+axt2p = figt2p.add_subplot(111,projection='3d')
+
+
+axt2p.plot_trisurf(posx2,posy2,temp2Per, cmap = 'plasma')
+axt2p.set_xlabel("x [m]")
+axt2p.set_ylabel("y [m]")
+axt2p.set_zlabel("Temperatura")
+plt.savefig("temp2500sPeriodica.png")
+
+#############################################################
+##########################################
+figta = plt.figure()
+axta = figta.add_subplot(111,projection='3d')
+
+
+axta.plot_trisurf(posx,posy,tempAb, cmap = 'plasma')
+axta.set_xlabel("x [m]")
+axta.set_ylabel("y [m]")
+axta.set_zlabel("Temperatura")
+plt.savefig("temp0sAbierto.png")
+
+
+
+figt1a = plt.figure()
+axt1a = figt1a.add_subplot(111,projection='3d')
+
+
+axt1a.plot_trisurf(posx1,posy1,temp1Ab,cmap = 'plasma')
+axt1a.set_xlabel("x [m]")
+axt1a.set_ylabel("y [m]")
+axt1a.set_zlabel("Temperatura")
+plt.savefig("temp100sAbierto.png")
+
+
+figt2a = plt.figure()
+axt2a = figt2a.add_subplot(111,projection='3d')
+
+
+axt2a.plot_trisurf(posx2,posy2,temp2Ab, cmap = 'plasma')
+axt2a.set_xlabel("x [m]")
+axt2a.set_ylabel("y [m]")
+axt2a.set_zlabel("Temperatura")
+plt.savefig("temp2500sAbierto.png")
+######################################################
+
+
+pro1 = promeds[:,0]
+pro2 = promeds[:,1]
+pro3 = promeds[:,2]
+
+
+plt.figure()
+plt.plot(pro1, c='r', label ='Fijas')
+plt.plot(pro2, c='g', marker ='o', label ='Periodicas')
+plt.plot(pro3, c='b', label ='Abiertas')
+plt.title("Temperatura promedio")
+plt.xlabel("tiempo*10 [s]")
+plt.ylabel("Temperatura [C]")
+plt.tight_layout()
+plt.legend()
+plt.savefig("TemperaturasPromedio.png")
+
+
+
+
+
+
+
+
+
+
+
